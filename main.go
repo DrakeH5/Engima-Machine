18edbba266a36b29118463d6864378c0cdc15409
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2022-11-05 15:01:38 -0500

    scrambling (plug board)

--- main.go
+++ main.go
@@ -7,11 +7,25 @@ import (
 )
 
 func main() {
+	var message string
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println("Input:")
 	scanner.Scan()
 	input := scanner.Text()
-	var message string = input
+	for i := 0; i < len(input); i++ {
+		message += scrambler(string(input[i]))
+	}
 	fmt.Println(message)
+}
 
+func scrambler(inputedLetter string) string {
+	scrambles := map[interface{}]interface{}{
+		"h": "q",
+		"i": "p",
+	}
+	if scrambles[inputedLetter] != nil {
+		return scrambles[inputedLetter].(string)
+	} else {
+		return inputedLetter
+	}
 }
