31b0a37da68555c1c380b7e5416b76bd6538c167
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2022-11-06 14:40:47 -0600

    reflector

--- main.go
+++ main.go
@@ -80,18 +80,64 @@ func RemoveIndex(s []string, index int) []string {
 	return append(s[:index], s[index+1:]...)
 }
 
+func generateReflector() map[interface{}]interface{} {
+	letters := [26]string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+	unusedLetters := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+	reflector := map[interface{}]interface{}{
+		"a": "",
+		"b": " ",
+		"c": " ",
+		"d": " ",
+		"e": " ",
+		"f": " ",
+		"g": " ",
+		"h": " ",
+		"i": " ",
+		"j": " ",
+		"k": " ",
+		"l": " ",
+		"m": " ",
+		"n": " ",
+		"o": " ",
+		"p": " ",
+		"q": " ",
+		"r": " ",
+		"s": " ",
+		"t": " ",
+		"u": " ",
+		"v": " ",
+		"w": " ",
+		"x": " ",
+		"y": " ",
+		"z": " ",
+	}
+	for i := 0; i < 26; i++ {
+		var randNmb int = rand.Intn(len(unusedLetters))
+		reflector[letters[i]] = unusedLetters[randNmb]
+		unusedLetters = RemoveIndex(unusedLetters[:], randNmb)
+	}
+	return reflector
+}
+
 func goThroughRotors(rotors [3]map[interface{}]interface{}, input string) string {
 	var output string
+	var reflector map[interface{}]interface{} = generateReflector()
 	for i := 0; i < len(input); i++ {
-		output += rotorConversions(rotors, string(input[i]))
+		output += rotorConversions(rotors, string(input[i]), reflector)
 	}
 	return output
 }
 
-func rotorConversions(rotors [3]map[interface{}]interface{}, input string) string {
+func rotorConversions(rotors [3]map[interface{}]interface{}, input string, reflector map[interface{}]interface{}) string {
 	var output string
 	for i := 0; i < 3; i++ {
 		output = rotors[i][input].(string)
+		if i == 2 {
+			output = reflector[input].(string)
+			for i := 2; i > -1; i-- {
+				output = rotors[i][input].(string)
+			}
+		}
 	}
 	return output
 }
